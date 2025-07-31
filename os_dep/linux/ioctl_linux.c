c86d3009e2ec21405eaf1818fda096fc1e6cc8c4
Nikolas <nikolas.hagelstein@gmail.com>
2025-07-31 19:46:25 +0200

    [FEATURE] Remove rtw_ioctil

--- os_dep/linux/ioctl_linux.c
+++ os_dep/linux/ioctl_linux.c
@@ -8965,35 +8965,3 @@ static int rtw_ioctl_wext_private(struct net_device *dev, struct ifreq *rq)
 		return rtw_ioctl_standard_wext_private(dev, rq);
 }
 
-int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd)
-{
-	struct iwreq *wrq = (struct iwreq *)rq;
-	int ret = 0;
-
-	switch (cmd) {
-	case RTL_IOCTL_WPA_SUPPLICANT:
-		ret = wpa_supplicant_ioctl(dev, &wrq->u.data);
-		break;
-#ifdef CONFIG_AP_MODE
-	case RTL_IOCTL_HOSTAPD:
-		ret = rtw_hostapd_ioctl(dev, &wrq->u.data);
-		break;
-#ifdef CONFIG_WIRELESS_EXT
-	case SIOCSIWMODE:
-		ret = rtw_wx_set_mode(dev, NULL, &wrq->u, NULL);
-		break;
-#endif
-#endif /* CONFIG_AP_MODE */
-	case SIOCDEVPRIVATE:
-		ret = rtw_ioctl_wext_private(dev, rq);
-		break;
-	case (SIOCDEVPRIVATE+1):
-		ret = rtw_android_priv_cmd(dev, rq, cmd);
-		break;
-	default:
-		ret = -EOPNOTSUPP;
-		break;
-	}
-
-	return ret;
-}
